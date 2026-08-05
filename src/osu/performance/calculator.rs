13c1783d4987bc87ea68642478e0b88444d29075
Q2ddz <102681680+Q2ddz@users.noreply.github.com>
2026-08-04 19:57:59 -0500

    Update calculator.rs

--- src/osu/performance/calculator.rs
+++ src/osu/performance/calculator.rs
@@ -17,7 +17,7 @@ use crate::{
 };
 
 // * This is being adjusted to keep the final pp value scaled around what it used to be when changing things.
-pub const PERFORMANCE_BASE_MULTIPLIER: f64 = 1.89;
+pub const PERFORMANCE_BASE_MULTIPLIER: f64 = 1.70;
 
 pub(super) struct OsuPerformanceCalculator<'mods> {
     attrs: OsuDifficultyAttributes,
@@ -109,7 +109,7 @@ impl OsuPerformanceCalculator<'_> {
                 + f64::from(self.state.hitresults.n50) * n50_mult)
                 .min(total_hits);
 
-            multiplier /= 2.19;
+            multiplier /= 1.97;
         }
 
         let speed_deviation = self.calculate_speed_deviation();
